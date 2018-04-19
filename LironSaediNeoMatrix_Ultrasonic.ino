1ec2d266a7beff5fea46039ccd1590a37a54d4b9
LironSaedi <lionsaedi@gmail.com>
2018-04-18 17:31:42 -0700

    first commit

---
+++ LironSaediNeoMatrix_Ultrasonic.ino
@@ -0,0 +1,40 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_SPITFT_Macros.h>
+#include <gfxfont.h>
+
+#include <Adafruit_NeoMatrix.h>
+#include <Ultrasonic.h>
+
+Ultrasonic sensor(4, 3);
+Adafruit_NeoMatrix matrix(8, 8, 2);
+
+void flash
+{
+
+}
+void setup()
+{
+  matrix.begin();
+  matrix.setBrightness(30);
+  matrix.clear();
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  long distanceFromSensor = sensor.Ranging(0);
+  delay(1);
+  Serial.println(distanceFromSensor);
+  int value = map(distanceFromSensor, 0, 50, 0, 255);
+  Serial.println(value);
+  
+  for (int i = 0; i < 8; i++)
+  {
+    for (int j = 0; j < 8; j++)
+    {
+      matrix.drawPixel(i, j, matrix.Color(255-value, 255-value, 0));
+    }
+  }
+  matrix.show();
+}
